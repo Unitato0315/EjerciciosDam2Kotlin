53de5c2fcdd06001183defc6efe8bb7e17ac25b0
Unitato0315 <jvsonic9@gmail.com>
2023-10-09 18:38:10 +0200

    Añado ventanas sin pasar datos

--- VariosActivitysConDatos/app/src/main/java/com/example/variosactivityspasardatos/MainActivity.kt
+++ VariosActivitysConDatos/app/src/main/java/com/example/variosactivityspasardatos/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.variosactivityspasardatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.variosactivityspasardatos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.boton.setOnClickListener {
+            irVentana()
+        }
+    }
+
+    private fun irVentana() {
+        var sIntent: Intent = Intent(this, Ventana2::class.java)
+        startActivity(sIntent)
     }
 }
