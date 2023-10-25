58939850f63a2a08a8b3a398fc393eb7cc8546d6
Jose Vicente Vargas Mestanza <jvsonic9@gmail.com>
2023-10-25 11:39:51 +0200

    simonDice funcional

--- SimonDice/app/src/main/java/com/example/simondice/MainActivity.kt
+++ SimonDice/app/src/main/java/com/example/simondice/MainActivity.kt
@@ -3,46 +3,141 @@ package com.example.simondice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
+import android.widget.TextView
 import com.example.simondice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    var aleatorios= arrayListOf<Int>()
-    var elecJugador= arrayListOf<Int>()
-    var nivel = 1
-
-    var timer = object: CountDownTimer((nivel * 2 * 800).toLong(), 800){
-        override fun onTick(millisUntilFinished: Long) {
-            binding.tvLevel.text= "seconds remaining: " + millisUntilFinished / 1000
-        }
-
-        override fun onFinish() {
-            binding.tvLevel.text= "prueba"
-        }
-    }
+    var patron = mutableListOf<Int>()
+    var nivel = 0
+    var colores = mutableListOf<TextView>()
+    var cont = 0
+    var cont2 = 0
+    var contRespuestas = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var colores= listOf(binding.etRojo,binding.etYellow,binding.etVerde,binding.etBlue)
-        binding.tvLevel.text="Nivel $nivel"
+        colores.addAll(listOf(binding.etRojo,binding.etVerde,binding.etYellow,binding.etBlue))
+        binding.tvLevel.visibility=View.INVISIBLE
+        binding.tvResul.visibility=View.INVISIBLE
         binding.btnSiguiente.text="Iniciar"
 
-
         binding.btnSiguiente.setOnClickListener {
-            timer.start()
+            this.nivel++
+            patron.add((1..4).random())
+            contRespuestas=0
+            iniciarPatron()
+            binding.tvLevel.visibility=View.VISIBLE
+            binding.tvResul.visibility=View.INVISIBLE
+            binding.tvLevel.text="Nivel $nivel"
         }
 
         binding.btnReiniciar.setOnClickListener {
-
+            nivel = 1
+            binding.tvLevel.visibility=View.INVISIBLE
+            binding.btnSiguiente.text="Iniciar"
+            binding.btnSiguiente.isEnabled=true
+            patron.clear()
         }
 
         for (color in colores){
             color.setOnClickListener {
-
+                comprobarEleccion(color)
             }
         }
     }
 
+    fun comprobarEleccion(color: TextView){
+        if(patron[contRespuestas].compareTo(color.contentDescription.toString().toInt())==0){
+            for (color in colores){
+                color.isEnabled = false
+            }
+            var timer = object: CountDownTimer((2 * 800).toLong(), 800){
+                override fun onTick(millisUntilFinished: Long) {
+                    if(cont%2 == 0){
+
+                        when(patron[contRespuestas]){
+                            1 -> colores[0].setBackgroundColor(getColor(R.color.red))
+                            2 -> colores[1].setBackgroundColor(getColor(R.color.green))
+                            3 -> colores[2].setBackgroundColor(getColor(R.color.yellow))
+                            4 -> colores[3].setBackgroundColor(getColor(R.color.blue))
+                        }
+                    }else{
+                        when(patron[contRespuestas]){
+                            1 -> colores[0].setBackgroundColor(getColor(R.color.redPalid))
+                            2 -> colores[1].setBackgroundColor(getColor(R.color.greenPalid))
+                            3 -> colores[2].setBackgroundColor(getColor(R.color.yellowPalid))
+                            4 -> colores[3].setBackgroundColor(getColor(R.color.bluePalid))
+                        }
+                    }
+                    cont++
+                }
+
+                override fun onFinish() {
+                    cont = 0
+                    for (color in colores){
+                        color.isEnabled = true
+                    }
+                    contRespuestas++;
+                    if (contRespuestas == patron.size){
+                        for (color in colores){
+                            color.isEnabled = false
+                        }
+                        binding.tvResul.visibility=View.VISIBLE
+                        binding.tvResul.text= "Has acertado puedes pasar al siguente nivel"
+                        binding.btnSiguiente.isEnabled=true
+                    }
+                }
+            }
+            timer.start()
+
+        }else{
+            binding.tvResul.visibility=View.VISIBLE
+            binding.tvResul.text= "Te has equivocado vuelve a intentarlo"
+            contRespuestas=0
+            iniciarPatron()
+        }
+    }
+    fun iniciarPatron(){
+        for (color in colores){
+            color.isEnabled = false
+        }
+        binding.btnReiniciar.isEnabled=false
+        binding.btnSiguiente.isEnabled=false
+
+        var timer = object: CountDownTimer((nivel * 2 * 800).toLong(), 800){
+            override fun onTick(millisUntilFinished: Long) {
+                if(cont%2 == 0){
+                    when(patron[cont2]){
+                        1 -> colores[0].setBackgroundColor(getColor(R.color.red))
+                        2 -> colores[1].setBackgroundColor(getColor(R.color.green))
+                        3 -> colores[2].setBackgroundColor(getColor(R.color.yellow))
+                        4 -> colores[3].setBackgroundColor(getColor(R.color.blue))
+                    }
+                }else{
+                    when(patron[cont2]){
+                        1 -> colores[0].setBackgroundColor(getColor(R.color.redPalid))
+                        2 -> colores[1].setBackgroundColor(getColor(R.color.greenPalid))
+                        3 -> colores[2].setBackgroundColor(getColor(R.color.yellowPalid))
+                        4 -> colores[3].setBackgroundColor(getColor(R.color.bluePalid))
+                    }
+                    cont2++
+                }
+                cont++
+            }
+
+            override fun onFinish() {
+                cont = 0
+                cont2 = 0
+                for (color in colores){
+                    color.isEnabled = true
+                }
+                binding.btnReiniciar.isEnabled=true
+            }
+        }
+        timer.start()
+    }
 }
